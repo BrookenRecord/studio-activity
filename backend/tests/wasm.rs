25bfae0b6fa8b92f996879e2d6155a2bc5e4d0f6
Brooke Rhodes <brhodes@roblox.com>
2026-04-05 22:03:41 +0100

    dont test on deploy

--- backend/tests/wasm.rs
+++ backend/tests/wasm.rs
@@ -1,10 +1,22 @@
 #![cfg(target_arch = "wasm32")]
 
+#[path = "wasm/helpers.rs"]
 pub mod helpers;
 
+#[path = "wasm/error.rs"]
 pub mod error;
+
+#[path = "wasm/extractors.rs"]
 pub mod extractors;
+
+#[path = "wasm/middleware.rs"]
 pub mod middleware;
+
+#[path = "wasm/ping.rs"]
 pub mod ping;
+
+#[path = "wasm/redirect.rs"]
 pub mod redirect;
+
+#[path = "wasm/routing.rs"]
 pub mod routing;
